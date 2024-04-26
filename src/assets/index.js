a562da8e7ade86ebdfd16452ef79f6d58439f31c
James Castillo <rubenjamescastillogarcia12@gmail.com>
2024-04-25 18:41:53 -0600

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import mysql from "./mysql.png"
+import link from "./link.png"
+import socialize from "./socialize.png"
+import netflix from "./netflix.jpg"
+import ecommerce from "./ecommerce.jpg"
+import buscador from "./buscador.jpg"
+import linkedin from "./linkedin.png"
+import githublink from "./githublink.png"
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import bootstrap from "./tech/bootstrap.png"
+import angular from "./tech/angular.png"
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  mysql,
+  bootstrap,
+  link,
+  angular,
+  socialize,
+  netflix,
+  ecommerce,
+  buscador,
+  linkedin,
+  githublink
+};
---
+++ src/constants/index.js
@@ -0,0 +1,258 @@
+import {
+  mobile,
+  backend,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  github,
+  mysql,
+  bootstrap,
+  angular,
+  socialize,
+  netflix,
+  ecommerce,
+  buscador,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "MySQL",
+    icon: mysql,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "github",
+    icon: github,
+  },
+  {
+    name: "bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "angular",
+    icon: angular,
+  },
+];
+
+const experiences = [
+  {
+    title: "Desarrollador web",
+    company_name: "Centro Comercial Mercantil del Llano S.A. de C.V.",
+    icon: web,
+    iconBg: "#383E56",
+    date: "Enero 2023 - Septiembre 2023",
+    points: [
+      "Desarrollo de una aplicación web en HTML5, CSS3, Javascript y PHP",
+      "Mantenimiento de la aplicación web",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "SocializeX",
+    description:
+      "Una red social en línea que conecta a usuarios, permitiéndoles compartir publicaciones así como interactuar con amigos y seguir a personas de interés. ",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Appwrite",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Typescript",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: socialize,
+    source_code_link: "https://github.com/James-Castillo/SocializeX",
+    web_code_link: "https://socialize-g1nhwyhpa-james-castillos-projects.vercel.app/",
+  },
+  {
+    name: "Netflix-Clone",
+    description:
+      "Un clon de Netflix creado con fines educativos, utilizando una API REST y autenticación de Google para acceder al contenido de forma segura.",
+    tags: [
+      {
+        name: "Angular",
+        color: "red-text-gradient",
+      },
+      {
+        name: "RestApi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Typescript",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: netflix,
+    source_code_link: "https://github.com/James-Castillo/Netflix-Clone",
+    web_code_link: "https://netflix-clone-mu-swart.vercel.app/",
+  },
+  {
+    name: "E-commerce",
+    description:
+      "Una plataforma de comercio electrónico diseñada con propósitos educativos, implementando una API REST para la gestión de productos.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "RestApi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "yellow-text-gradient",
+      },
+      
+    ],
+    image: ecommerce,
+    source_code_link: "https://github.com/James-Castillo/e-commerce-react",
+    web_code_link: "https://easy-market-react.netlify.app/",
+  },
+  {
+    name: "Buscador de películas",
+    description:
+      "Una plataforma de películas diseñada con propósitos educativos, implementando una API REST para la busqueda de películas.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "RestApi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: buscador,
+    source_code_link: "https://github.com/James-Castillo/buscador-peliculas-react",
+    web_code_link: "https://buscador-pelicula-react.netlify.app/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
